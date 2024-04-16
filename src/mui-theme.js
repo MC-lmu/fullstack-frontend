a892ad26af0a36680bd7ed7e827a202c72290fc9
Mathieu CHOPLAIN <mathieu.choplain.etu@univ-lemans.fr>
2024-04-16 17:52:57 +0200

    change colors of theme

--- src/mui-theme.js
+++ src/mui-theme.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-unused-vars */
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { deepPurple, green, red } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: deepPurple[700],
     },
     secondary: {
-      main: '#19857b',
+      main: green[400],
     },
     error: {
       main: red.A400,
