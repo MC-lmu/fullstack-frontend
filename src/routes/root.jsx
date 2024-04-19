3a1676ca9cd442ccb8e47d9db59a66e0d9d487dc
Mathieu CHOPLAIN <mathieu.choplain.etu@univ-lemans.fr>
2024-04-19 11:27:23 +0200

    fixup:

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,6 @@
 import Container from '@mui/material/Container';
 import { Outlet } from 'react-router-dom';
-import Topbar from './topbar';
+import Topbar from '../components/topbar';
 
 export default function Root(props) {
   return (
